2ba1f87ffe16947f780adf9e4dc2bd122b05bbda
salmansrabon <salmansrabon@gmail.com>
2025-12-26 15:40:57 +0600

    practice components

--- src/test/java/MyJunit.java
+++ src/test/java/MyJunit.java
@@ -1,15 +1,17 @@
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MyJunit {
@@ -37,7 +39,7 @@ public class MyJunit {
         driver.findElement(By.id("password")).sendKeys("1234");
         driver.findElement(By.cssSelector("[type=submit]")).click();
         Cookies utils=new Cookies(driver);
-        utils.setCookie();
+        utils.getCookie();
     }
 
     @Test
@@ -71,5 +73,101 @@ public class MyJunit {
             }
         }
 
+    }
+    @Test
+    @Order(4)
+    @DisplayName("Button click manipulation")
+    public void buttonClick(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
+        Actions actions=new Actions(driver);
+        actions.doubleClick(driver.findElements(By.tagName("button")).get(6)).click().perform();
+        driver.switchTo().alert().accept();
+        actions.contextClick(driver.findElements(By.tagName("button")).get(7)).click().perform();
+        driver.switchTo().alert().accept();
+        actions.moveToElement(driver.findElements(By.tagName("button")).get(7)).sendKeys(Keys.ENTER).perform();
+        driver.switchTo().alert().accept();
+    }
+    @Test
+    @Order(5)
+    @DisplayName("NextJS datetime manipulation")
+    public void dateTime(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+        WebElement dateTimeElem=driver.findElement(By.cssSelector("[type='datetime-local']"));
+        dateTimeElem.click();
+        dateTimeElem.sendKeys("24","Jan");
+        dateTimeElem.sendKeys(Keys.ARROW_RIGHT,"2026");
+        dateTimeElem.sendKeys(Keys.ARROW_RIGHT,"3","00","AM");
+
+    }
+    @Test
+    @Order(6)
+    @DisplayName("ReactJS readonly date manipulation")
+    public void reactDateTime(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+        WebElement dateInput= driver.findElements(By.className("form-control")).get(1);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].removeAttribute('readonly')", dateInput);
+        // Set date value
+        js.executeScript("arguments[0].value='11/24/2025'", dateInput);
+    }
+    @Test
+    @Order(7)
+    @DisplayName("Handle new tab")
+    public void openNewTab(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+        driver.findElement(By.id("newTabBtn")).click();
+        ArrayList<String> w = new ArrayList(driver.getWindowHandles());
+        //switch to open tab
+        driver.switchTo().window(w.get(1));
+        String newTabTitle=driver.getTitle();
+        Assertions.assertEquals(newTabTitle,"Example Domain");
+        driver.close();
+        driver.switchTo().window(w.get(0));
+    }
+    @Test
+    @Order(8)
+    @DisplayName("Handle new window")
+    public void handleNewWindow(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+        driver.findElement(By.id(("newWindowBtn"))).click();
+        String mainWindow = driver.getWindowHandle();
+        Set<String> allWindows = driver.getWindowHandles();
+
+        Assertions.assertTrue(allWindows.size() > 1, "New window did not open!");
+        // Just find the child window
+        for (String window : allWindows) {
+            if (!window.equals(mainWindow)) {
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+        driver.switchTo().window(mainWindow);
+    }
+    @Test
+    @Order(9)
+    @DisplayName("Handle Modal")
+    public void handleModal(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+        driver.findElement(By.id("openModalBtn")).click();
+        driver.findElements(By.className("btn-primary")).get(1).click();
+        String message= driver.findElement(By.cssSelector(".mt-2.text-success.fw-semibold")).getText();
+        System.out.println(message);
+    }
+    @Test
+    @Order(10)
+    @DisplayName("Handle Iframe")
+    public void handleIframe(){
+        driver.get("http://localhost:3000/dashboard/practice-components");
+        WebElement frame=driver.findElement(By.tagName("iframe"));
+        driver.switchTo().frame(frame);
+        //driver.switchTo().frame("embeddedExample");
+        String text= driver.findElements(By.tagName("p")).get(0).getText();
+        System.out.println(text);
+        Assertions.assertTrue(text.contains("Login to access your dashboard and users table"));
+        driver.switchTo().defaultContent();
     }
 }
